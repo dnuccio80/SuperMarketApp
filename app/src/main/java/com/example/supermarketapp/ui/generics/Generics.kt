25894189d70842c284ade8c29d26cd9390055f9a
dnuccio80 <nuccio227@gmail.com>
2025-11-11 22:15:27 -0300

    - Finished UI

--- app/src/main/java/com/example/supermarketapp/ui/generics/Generics.kt
+++ app/src/main/java/com/example/supermarketapp/ui/generics/Generics.kt
@@ -1,24 +1,64 @@
 package com.example.supermarketapp.ui.generics
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.text.isDigitsOnly
 
 @Composable
-fun TitleText(text:String, color:Color = Color.White) {
+fun TitleText(text: String, color: Color = Color.White) {
     Text(text, fontSize = 24.sp, fontWeight = FontWeight.ExtraBold, color = color)
 }
 
 @Composable
-fun SubtitleText(text:String, color:Color = Color.White) {
+fun SubtitleText(text: String, color: Color = Color.White) {
     Text(text, fontSize = 18.sp, fontWeight = FontWeight.SemiBold, color = color)
 }
 
-
+@Composable
+fun BodyText(text: String, color: Color = Color.White) {
+    Text(text, fontSize = 14.sp, fontWeight = FontWeight.Normal, color = color)
+}
 
 @Composable
-fun BodyText(text:String, color:Color = Color.White) {
-    Text(text, fontSize = 14.sp, fontWeight = FontWeight.Normal, color = color)
+fun TextFieldItem(
+    value: String,
+    label: String,
+    isInt: Boolean = false,
+    onValueChange: (String) -> Unit
+) {
+    TextField(
+        value = value,
+        onValueChange = {
+            if (isInt) {
+                if (!it.isDigitsOnly()) return@TextField
+                onValueChange(it)
+            } else {
+                onValueChange(it)
+            }
+        },
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp),
+        colors = TextFieldDefaults.colors(
+            focusedContainerColor = Color.DarkGray.copy(alpha = .2f),
+            unfocusedContainerColor = Color.Transparent,
+            focusedTextColor = Color.White,
+            unfocusedTextColor = Color.White
+        ),
+        singleLine = true,
+        keyboardOptions = if (isInt) KeyboardOptions(keyboardType = KeyboardType.Number) else KeyboardOptions.Default,
+        maxLines = 1,
+        label = { BodyText(label) }
+    )
 }
--- app/src/main/java/com/example/supermarketapp/ui/screens/addproduct/AddProductScreen.kt
+++ app/src/main/java/com/example/supermarketapp/ui/screens/addproduct/AddProductScreen.kt
@@ -4,6 +4,7 @@ import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.result.contract.ActivityResultContracts.GetContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -15,8 +16,12 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.windowInsetsEndWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
@@ -27,10 +32,14 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.core.text.isDigitsOnly
+import coil.compose.AsyncImage
 import com.example.supermarketapp.ui.generics.BodyText
 import com.example.supermarketapp.ui.generics.SubtitleText
+import com.example.supermarketapp.ui.generics.TextFieldItem
 import com.example.supermarketapp.ui.generics.TitleText
 
 @Composable
@@ -57,6 +66,21 @@ fun AddProductScreen(innerPadding: PaddingValues) {
             verticalArrangement = Arrangement.spacedBy(16.dp),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 32.dp, horizontal = 16.dp),
+                contentAlignment = Alignment.CenterStart
+            ) {
+                Icon(
+                    Icons.AutoMirrored.Filled.ArrowBack,
+                    contentDescription = "Back button",
+                    tint = Color.White,
+                    modifier = Modifier.clickable {
+                        // Back to Home Screen
+                        TODO()
+                    })
+            }
             Spacer(modifier = Modifier.weight(1f))
             TitleText("Añadir un producto")
             TextFieldItem(value = nameProduct, label = "Nombre")
@@ -80,6 +104,14 @@ fun AddProductScreen(innerPadding: PaddingValues) {
             ) {
                 SubtitleText("Añadir imagen")
             }
+            if (imageUri.isNotBlank()) {
+                AsyncImage(
+                    model = imageUri,
+                    contentDescription = "",
+                    modifier = Modifier.size(200.dp),
+                    contentScale = ContentScale.Crop
+                )
+            }
             Spacer(modifier = Modifier.weight(1f))
             Button(
                 onClick = { },
@@ -96,34 +128,3 @@ fun AddProductScreen(innerPadding: PaddingValues) {
 
 }
 
-@Composable
-private fun TextFieldItem(
-    value: String,
-    label: String,
-    isInt: Boolean = false,
-    onValueChange: (String) -> Unit
-) {
-    TextField(
-        value = value,
-        onValueChange = {
-            if (isInt) {
-                if (!it.isDigitsOnly()) return@TextField
-                onValueChange(it)
-            } else {
-                onValueChange(it)
-            }
-        },
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(horizontal = 16.dp),
-        colors = TextFieldDefaults.colors(
-            focusedContainerColor = Color.DarkGray.copy(alpha = .2f),
-            unfocusedContainerColor = Color.Transparent,
-            focusedTextColor = Color.White,
-            unfocusedTextColor = Color.White
-        ),
-        singleLine = true,
-        maxLines = 1,
-        label = { BodyText(label) }
-    )
-}
